105e19361a7e48c0c9fe4c543e2a8babe104eb8d
simonghx <simongheux@gmail.com>
2018-04-16 11:11:37 +0200

    exo 3 et 4

--- src/components/Exo2.vue
+++ src/components/Exo2.vue
@@ -1,5 +1,7 @@
 <template>
-  <button class="btn" :class="{'btn-primary' : !change, 'btn-danger' : change}" @click.prevent="changeButton">Click on me</button>
+  <section id="exo2" class="my-5 panel active">
+    <button class="btn" :class="{'btn-primary' : !change, 'btn-danger' : change}" @click.prevent="changeButton">Click on me</button>
+  </section>
 </template>
 
 <script>
